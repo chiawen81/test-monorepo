41904fef672ca00504fc8e0cb4300a9ac0a84542
chiawen81 <fangchiawen@gmail.com>
2024-10-26 23:59:52 +0800

    Auto-generated commit

--- your-app/redirect.js
+++ your-app/redirect.js
@@ -1,20 +1,19 @@
-var pathSegmentsToKeep = 3;
-// NOTE:
-// project ex:`your-app` 在第幾層目錄
-// 目前是第二層=> https://chiawen81.github.io/test-monorepo/your-app/
+
+var pathSegmentsToKeep = 2;
+// NOTE:專案在第幾層目錄
 
 var allowedPathSegments = ['your-app'];
 // NOTE:目前有的 project
 
 var l = window.location;
-var pathSegments = l.pathname.split('/');
+var pathSegments = l.pathname.split('/') - 1;
+// NOTE:去除網域後面接的 "/"
 
 var shouldRedirect =
   (pathSegments.length > pathSegmentsToKeep) &&
   (allowedPathSegments.indexOf(pathSegments[pathSegmentsToKeep - 1]) !== -1);
 
 if (shouldRedirect) {
-  window.location.replace(
-    l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + '/test-monorepo/your-app/' + l.search + l.hash
-  );
+  let url = l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + '/test-monorepo/your-app/' + l.search + l.hash;
+  window.location.replace(url);
 }
