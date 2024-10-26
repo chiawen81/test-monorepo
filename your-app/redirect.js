c7b9c67bfc7da1b1caedf6f65bb1c5995f5deae8
chiawen81 <fangchiawen@gmail.com>
2024-10-27 00:13:52 +0800

    Auto-generated commit

--- your-app/redirect.js
+++ your-app/redirect.js
@@ -1,24 +1,41 @@
+(function () {
+  console.log(11111111)
+  console.log(location.pathname)
+  console.log(location)
+  alert("11111111")
+  sessionStorage.redirect = location.href;
+  var redirectTo = "/test-monorepo/your-app/";
 
-var projectLevek = 2;
-// NOTE:專案在第幾層目錄
-var pathSegmentsToKeep = 2 + 1;
-// NOTE:加上網域後面接的 "/"
-var allowedPathSegments = ['your-app'];
-// NOTE:目前有的 project
+  if (location.pathname.startsWith("/your-app/")) {
+    alert("222222")
+    redirectTo = "/test-monorepo/your-app/";
+  };
 
-var l = window.location;
-var pathSegments = l.pathname.split('/');
+  location.replace(redirectTo);
+})();
 
-var shouldRedirect =
-  (pathSegments.length > pathSegmentsToKeep) &&
-  (allowedPathSegments.indexOf(pathSegments[pathSegmentsToKeep - 1]) !== -1);
 
-if (shouldRedirect) {
-  let url = l.protocol + '//' + l.hostname +
-    (l.port ? ':' + l.port : '') +
-    '/test-monorepo/your-app/' +
-    pathSegments.slice(pathSegmentsToKeep).join('/') +  // 保留未找到的路徑部分
-    (l.search || '') +
-    (l.hash || '');
-  window.location.replace(url);
-}
+
+// var projectLevek = 2;
+// // NOTE:專案在第幾層目錄
+// var pathSegmentsToKeep = 2 + 1;
+// // NOTE:加上網域後面接的 "/"
+// var allowedPathSegments = ['your-app'];
+// // NOTE:目前有的 project
+
+// var l = window.location;
+// var pathSegments = l.pathname.split('/');
+
+// var shouldRedirect =
+//   (pathSegments.length > pathSegmentsToKeep) &&
+//   (allowedPathSegments.indexOf(pathSegments[pathSegmentsToKeep - 1]) !== -1);
+
+// if (shouldRedirect) {
+//   let url = l.protocol + '//' + l.hostname +
+//     (l.port ? ':' + l.port : '') +
+//     '/test-monorepo/your-app/' +
+//     pathSegments.slice(pathSegmentsToKeep).join('/') +  // 保留未找到的路徑部分
+//     (l.search || '') +
+//     (l.hash || '');
+//   window.location.replace(url);
+// }
